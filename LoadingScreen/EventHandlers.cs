e8e76c3f96f7e55ce45c6394ccd73212382637da
matteo francesconi <matteo.francesconi@davide.it>
2019-02-13 12:29:37 +0100

    first Commit: version 0.0.1 working TODO: add player rotation when implemented

---
+++ LoadingScreen/EventHandlers.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Linq;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace LoadingScreen
+{
+    public class EventHandlers : IEventHandlerPlayerJoin, IEventHandlerWaitingForPlayers, IEventHandlerRoundStart
+    {
+        private readonly LoadingScreen plugin;
+
+        private bool preRound = false;
+
+        public EventHandlers(LoadingScreen plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            if (preRound)
+            {
+                if (ev.Player.TeamRole.Team == Smod2.API.Team.NONE)
+                {
+                    Smod2.API.Door door = getRndDoor();
+                    ev.Player.Teleport(new Vector(door.Position.x + 1f, door.Position.y + 1f, door.Position.z + 1f),
+                        false);
+                }
+            }
+        }
+
+        public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+        {
+            plugin.RefreshConfig();
+            preRound = true;
+        }
+
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            preRound = false;
+        }
+
+        private Smod2.API.Door getRndDoor()
+        {
+            Random rnd = new Random();
+            Smod2.API.Door res;
+            if (plugin.doors[0] == "*")
+            {
+                res = plugin.Server.Map.GetDoors().OrderBy(x=>rnd.Next(100)).FirstOrDefault();
+            }
+            else
+            {
+                res = plugin.Server.Map.GetDoors().OrderBy(x=>rnd.Next(100)).FirstOrDefault(x => plugin.doors.Contains(x.Name));
+            }
+
+            return res;
+        }
+    }
+}
---
+++ LoadingScreen/LoadingScreen.cs
@@ -0,0 +1,55 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using Smod2.Config;
+using Smod2.Events;
+
+namespace LoadingScreen
+{
+    [PluginDetails(
+        name = "Loading Screen",
+        author = "The Matty",
+        description = "Improves \"waiting for players\" Screen by teleporting people to random doors",
+        id = "mattymatty.loadingscreen",
+        SmodMajor = 3,
+        SmodMinor = 2,
+        SmodRevision = 0,
+        version = "0.0.1"
+            )]
+    public class LoadingScreen : Plugin
+    {
+        public string[] doors;
+        public EventHandlers Handlers { get; private set; }
+
+        public override void Register()
+        {
+            
+            string[] defaultDoors =
+            {
+                "*"
+            };
+            AddConfig(new ConfigSetting("ls_doorId", defaultDoors, SettingType.LIST, true,
+                "Ranks allowed to adjust player Preferences."));
+
+            Handlers = new EventHandlers(this);
+
+            AddEventHandlers(Handlers, Priority.Low);
+        }
+
+        public void RefreshConfig()
+        {
+            doors = GetConfigList("ls_doorId");
+        }
+
+        public override void OnEnable()
+        {
+            Info("Loading Screen enabled!");
+        }
+
+        public override void OnDisable()
+        {
+            Info("Loading Screen disabled!");
+        }
+
+    }
+}
