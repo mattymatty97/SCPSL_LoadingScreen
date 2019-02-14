4867bde5696834747f6c0f8683c4b9c7f24cd91d
matteo francesconi <matteo.francesconi@davide.it>
2019-02-14 10:38:46 +0100

    version 0.0.2 optimized TODO: add player rotation when implemented

--- LoadingScreen/EventHandlers.cs
+++ LoadingScreen/EventHandlers.cs
@@ -11,11 +11,24 @@ namespace LoadingScreen
     {
         private readonly LoadingScreen plugin;
 
+        private Smod2.API.Door[] doors;
+        private Random rnd = new Random();
+
         private bool preRound = false;
 
         public EventHandlers(LoadingScreen plugin)
         {
             this.plugin = plugin;
+        }
+
+        public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+        {
+            plugin.RefreshConfig();
+            if (plugin.doors.Contains("*"))
+                doors = ev.Server.Map.GetDoors().ToArray();
+            else
+                doors = ev.Server.Map.GetDoors().Where(x => plugin.doors.Contains(x.Name.ToUpper())).ToArray();
+            preRound = true;
         }
 
         public void OnPlayerJoin(PlayerJoinEvent ev)
@@ -25,16 +38,10 @@ namespace LoadingScreen
                 if (ev.Player.TeamRole.Team == Smod2.API.Team.NONE)
                 {
                     Smod2.API.Door door = getRndDoor();
-                    ev.Player.Teleport(new Vector(door.Position.x + 1f, door.Position.y + 1f, door.Position.z + 1f),
-                        false);
+                    ev.Player.Teleport(new Vector(door.Position.x + 1f, door.Position.y + 1f, door.Position.z + 1f));
                 }
-            }
-        }
 
-        public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
-        {
-            plugin.RefreshConfig();
-            preRound = true;
+            }
         }
 
         public void OnRoundStart(RoundStartEvent ev)
@@ -44,18 +51,7 @@ namespace LoadingScreen
 
         private Smod2.API.Door getRndDoor()
         {
-            Random rnd = new Random();
-            Smod2.API.Door res;
-            if (plugin.doors[0] == "*")
-            {
-                res = plugin.Server.Map.GetDoors().OrderBy(x=>rnd.Next(100)).FirstOrDefault();
-            }
-            else
-            {
-                res = plugin.Server.Map.GetDoors().OrderBy(x=>rnd.Next(100)).FirstOrDefault(x => plugin.doors.Contains(x.Name));
-            }
-
-            return res;
+            return doors[rnd.Next(0, doors.Length)];
         }
     }
 }
